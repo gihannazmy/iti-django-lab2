410205d8046cd224e16d3c49bf9c7293eae170e4
gihannazmy <gihanelsayed187@gmail.com>
2024-08-26 16:33:07 +0300

    updting database

--- itian/itian/settings.py
+++ itian/itian/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'itian.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'itian',
+        'USER':'iti',
+        'PASSWORD':'123',
+        'HOST':'loaclhost',
     }
 }
 
